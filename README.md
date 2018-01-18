96b4206ca37c3095b1aa9718233d149bf1d57b7a
austinsturm <astor@astor.localdomain>
2018-01-18 00:37:45 -0500

    merged

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
+<<<<<<< HEAD
 # cop4331
 For Leineckers COP4331 projects
+=======
+# cop4331
+For Leineckers COP4331 projects
+
+## Locally setting up API - Linux or Mac
+
+1. Install PHP and MySQL locally - If you set a mysql root password, go to *api/.env* and set **DB_PASSWORD** to your mysql root password
+2. Create a local database named *smallproject*
+3. Go to the api folder and run the command `php artisan migrate`
+4. In the api folder run `./start` and a php server on **localhost:3000** will serve the api.
+5. Open browser, and go to localhost:3000
+>>>>>>> ac8b044485c53b2ef9b25758b6a4cddcce1b9757
---
+++ smallproject/api/routes/web.php
@@ -0,0 +1,29 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It is a breeze. Simply tell Lumen the URIs it should respond to
+| and give it the Closure to call when that URI is requested.
+|
+*/
+
+$app->get('/', function () use ($app) {
+    return $app->version();
+});
+
+$app->group(['prefix' => 'v1'], function($app)
+{
+    // User
+    $app->post('/register','UserController@create');
+    $app->post('/login','UserController@authenticate');
+    $app->post('/user/contacts','UserController@showContacts');
+
+    // Organizations
+    $app->post('/contact/add', 'ContactController@create');
+    $app->post('/contact/get/', 'ContactController@show');
+    $app->post('/contact/destroy', 'ContactController@destroy');
+});
