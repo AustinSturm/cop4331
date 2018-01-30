27346f4b7487d2086511002e8d974dc3ad90e7cb
ratio16180 <9160932+ratio16180@users.noreply.github.com>
2018-01-30 16:20:16 -0500

    Added session tracking

--- smallproject/web/static/main.js
+++ smallproject/web/static/main.js
@@ -2,12 +2,7 @@
 var enabled = 0;
 
 function navflip() {
-    if(enabled == 0 ){
         w3_open();
-    }
-    else if(enabled == 1){
-        w3_close();
-    }
 }
 
 function w3_open() {
@@ -63,8 +58,8 @@ function register() {
 }
 function logout() {
     document.cookie = "api_key=;";
-    document.getElementById("username").innerHTML = "Anonymous";
-    // Add a line to remove all of the previously loaded contacts... TODO
+    document.getElementById("username").innerHTML = "menu";
+    document.getElementById("nav-sidebar").innerHTML = "";
     loadContent("login.html");
 }
 
@@ -83,10 +78,10 @@ function login() {
 			password: g_password
 		},
 		success: function(result){
-         document.cookie = 'api_key=' + result.api_key + ' path=/'
-         api_key_G = result.api_key
-         document.getElementById("username").innerHTML = g_email;
-        	console.log(result);
+        document.cookie = 'api_key=' + result.api_key
+        api_key_G = result.api_key
+        document.getElementById("username").innerHTML = "menu";
+        console.log(result);
         renderContacts();
         loadContent("welcomeUser.html");
         },
@@ -97,62 +92,43 @@ function login() {
 });
 }
 
-function renderContacts() {
+/*
+function deleteContact() {
+
+    // MUST ADD BUTTONS TO THE CONTACT LIST IN ORDER TO DELETE THAT WILL CALL THIS FUNCTION,
+    // ALONG WITH THEIR RESPECTIVE ID's THAT ARE ASSIGNED!!! 
+    // waiting for the load contact page to be finished before proceeding
+
+    
+    var api_key_G = document.cookie.substring(8); // removes `api_key=`
+    alert(api_key);
 
     $.ajax({
-		url: "http://35.227.78.91/user/contacts",
-		type: 'post',
-		data: {
-			api_key: api_key_G
-		},
-		success: function(result){
+        url: "http://35.227.78.91/contact/destroy",
+        type: 'post',
+        data: {
+            api_key: api_key_G,
+            contact_id
+        }
+    })   
+}
+*/
+
+function renderContacts_reload(api_key_G) {
+
+    $.ajax({
+        url: "http://35.227.78.91/user/contacts",
+        type: 'post',
+        data: {
+            api_key: api_key_G
+        },
+        success: function(result){
          var contactResponse = "";
          $.each(result, function(index){
-            contactResponse += "loldongs";
+            contactResponse += "<a href='#' class='w3-bar-item w3-button w3-border' onclick='renderContact(result[index].ContactID)'>" + result[index].contact_name + "</a>";
          });
 
-        	document.getElementById("nav-sidebar").innerHTML = contactResponse;
-         console.log(result);
-    }});
-}
-
-function renderContact(id) {
-
-    $.ajax({
-		url: "http://35.227.78.91/contact/get",
-		type: 'post',
-		data: {
-			api_key: api_key_G,
-         ContactID: id
-		},
-		success: function(result){
-         response = `
-         <div class="w3-container contentCenter" style="max-width: 800px;">
-         	<form class='w3-padding w3-card-4 w3-light-grey'>
-         		<label>Name:</label></br>
-         		<b>${result.contact_name}</b></br></br>
-         		<label>Address:</label></br>
-         		<b>${result.contact_address}</b></br></br>
-         		<label>City:</label></br>
-         		<b>${result.contact_city}</b></br></br>
-         		<label>State:</label></br>
-         		<b>${result.contact_state}</b></br></br>
-         		<label>Zip:</label></br>
-         		<b>${result.contact_zip_code}</b></br></br>
-         		<label>Home Phone:</label></br>
-         		<b>${result.contact_home_phone}</b></br></br>
-         		<label>Cell Phone:</label></br>
-         		<b>${result.contact_cell_phone}</b></br></br>
-         		<label>Work Phone:</label></br>
-         		<b>${result.contact_work_phone}</b></br></br>
-         		<label>Primary Email:</label></br>
-         		<b>${result.contact_primary_email}</b></br></br>
-         		<label>Secondary Email:</label></br>
-         		<b>${result.contact_secondary_email}</b></br></br>
-         	</form>
-         </div>
-         `
-        	document.getElementById("content").innerHTML = response;
+            document.getElementById("nav-sidebar").innerHTML = contactResponse;
          console.log(result);
     }});
 }
@@ -168,7 +144,7 @@ function renderContacts() {
 		success: function(result){
          var contactResponse = "";
          $.each(result, function(index){
-            contactResponse += "<a href=\"#\" class=\"w3-bar-item w3-button w3-border\" onclick="renderContact(result[index].ContactID)">" + result[index].contact_name + "</a>";
+            contactResponse += "<a href='#' class='w3-bar-item w3-button w3-border' onclick='renderContact(result[index].ContactID)'>" + result[index].contact_name + "</a>";
          });
 
         	document.getElementById("nav-sidebar").innerHTML = contactResponse;
